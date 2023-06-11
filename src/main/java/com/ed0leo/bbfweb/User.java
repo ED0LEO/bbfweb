4d6aecd8a6ffb51efdf1147c15995c7f7e4cf182
ED0LEO <ed0leo@proton.me>
2023-06-11 21:08:52 +0200

    User entity has been added

---
+++ src/main/java/com/ed0leo/bbfweb/User.java
@@ -0,0 +1,18 @@
+package com.ed0leo.bbfweb;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "TBL_USERS")
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id private int id;
+    @Column private String firstName;
+    @Column private String lastName;
+    @Column private String username;
+    @Column private String password;
+    //Getters, setters, constructors and toString() method
+}

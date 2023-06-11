c72319034da6f5a484d9f979aceaef7608827449
ED0LEO <ed0leo@proton.me>
2023-06-11 20:48:47 +0200

    Initial project created with Web, JPA, MySQL

---
+++ src/main/java/com/ed0leo/bbfweb/BbfwebApplication.java
@@ -0,0 +1,13 @@
+package com.ed0leo.bbfweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbfwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbfwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ed0leo/bbfweb/BbfwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed0leo.bbfweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbfwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

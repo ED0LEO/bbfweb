ae0874e71646c2d841bab87700372ab8b43b8996
ED0LEO <ed0leo@proton.me>
2023-06-16 23:46:16 +0200

    Java filed and methods are updated

--- src/main/java/com/ed0leo/bbfweb/UserController.java
+++ src/main/java/com/ed0leo/bbfweb/UserController.java
@@ -1,6 +1,7 @@
 package com.ed0leo.bbfweb;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,38 +15,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ed0leo.bbfweb.User;
-import com.ed0leo.bbfweb.UserService;
-
 @RestController
 @RequestMapping(value = "user")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     @Autowired
     private UserService userService;
+
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
-    public User registerUser(@RequestBody User user) {
-        return this.userService.insert(user);
+    public User registerUser(@RequestBody User userVO) {
+
+        return this.userService.insert(userVO);
     }
+
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public List<User> findAllUser() {
+
         return this.userService.findAll();
     }
+
     @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public User findById(@PathVariable int id) {
         return this.userService.findById(id);
     }
+
     @PutMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
-    public User updateUser(@PathVariable int id, @RequestBody User user) {
+    public User updateUser(@PathVariable int id, @RequestBody User userVO) {
+
         return this.userService.updateUser(id, userVO);
     }
+
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable int id) {
+
         this.userService.delete(id);
     }
 }

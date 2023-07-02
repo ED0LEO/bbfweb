22a6864e05fac6d004c1bb770bc63ffcb09d7ef9
ED0LEO <ed0leo@proton.me>
2023-07-02 23:58:38 +0200

    Security config updated

---
+++ src/main/java/com/ed0leo/bbfweb/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ed0leo.bbfweb;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+}

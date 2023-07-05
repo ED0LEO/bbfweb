4fd3acde2785562f4b915ec5801d50b187ef54c4
ED0LEO <ed0leo@proton.me>
2023-07-05 23:36:42 +0200

    Cors disabled for development

--- src/main/java/com/ed0leo/bbfweb/BbfwebApplication.java
+++ src/main/java/com/ed0leo/bbfweb/BbfwebApplication.java
@@ -2,13 +2,13 @@ package com.ed0leo.bbfweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ed0leo.bbfweb")
 public class BbfwebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BbfwebApplication.class, args);
 	}
-
 }
--- src/main/java/com/ed0leo/bbfweb/WebSecurityConfig.java
+++ src/main/java/com/ed0leo/bbfweb/WebSecurityConfig.java
@@ -1,43 +1,43 @@
-package com.ed0leo.bbfweb;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
-public class WebSecurityConfig {
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("uuu").password("uuu").roles("USER").build());
-        return manager;
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/login").permitAll() // Allow access to the login page
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login") // Set the login page URL
+//package com.ed0leo.bbfweb;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class WebSecurityConfig {
+//
+////    @Bean
+////    public UserDetailsService userDetailsService() {
+////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////        manager.createUser(User.withDefaultPasswordEncoder().username("uuu").password("uuu").roles("USER").build());
+////        return manager;
+////    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("*").permitAll() // Allow access to the login page
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin((form) -> form
+//                        .loginPage("/login") // Set the login page URL
 //                        .defaultSuccessUrl("/users") // Set the URL to redirect after successful login
-                        .permitAll()
-                );
-//                .logout((logout) -> logout
-////                        .logoutUrl("/logout") // Set the logout URL
-////                        .logoutSuccessUrl("/login?logout") // Set the URL to redirect after successful logout
 //                        .permitAll()
 //                );
-
-        return http.build();
-    }
-}
+////                .logout((logout) -> logout
+//////                        .logoutUrl("/logout") // Set the logout URL
+//////                        .logoutSuccessUrl("/login?logout") // Set the URL to redirect after successful logout
+////                        .permitAll()
+////                );
+//
+//        return http.build();
+//    }
+//}

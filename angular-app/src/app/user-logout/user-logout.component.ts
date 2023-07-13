4b32becb6cd94b92d18e5c0e8cf6b8b5994c70b9
ED0LEO <ed0leo@proton.me>
2023-07-13 23:50:55 +0200

    Components' styles now unified and inconsistencies are fixed.

--- angular-app/src/app/user-logout/user-logout.component.ts
+++ angular-app/src/app/user-logout/user-logout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UserAccountService } from '../user-account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-logout',
@@ -9,18 +9,8 @@ import { UserAccountService } from '../user-account.service';
   `
 })
 export class UserLogoutComponent {
-  constructor(private userAccountService: UserAccountService) {
-    this.logout();
-  }
-
-  logout() {
-    this.userAccountService.logout().subscribe(
-      () => {
-        console.log('Logged out successfully');
-      },
-      (error) => {
-        console.log('Failed to logout', error);
-      }
-    );
+  constructor(
+   private router: Router) {
+    this.router.navigate(['/user/user-login']);
   }
 }

71e520fc0511408aad6c747dae272e804327f26a
ED0LEO <ed0leo@proton.me>
2023-07-18 23:40:37 +0200

    Reward video roll now does not cause reload

--- angular-app/src/app/components/rewards/rewards.component.ts
+++ angular-app/src/app/components/rewards/rewards.component.ts
@@ -12,10 +12,13 @@ interface Video {
   templateUrl: './rewards.component.html',
   styleUrls: ['./rewards.component.css']
 })
+// ... (imports and component decorator)
+
 export class RewardsComponent implements OnInit {
   videoList: Video[] = [];
+  videoIndex: number = 0;
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.fetchRecommendedVideos();
@@ -24,50 +27,29 @@ export class RewardsComponent implements OnInit {
   fetchRecommendedVideos(): void {
     // Replace 'YOUR_YOUTUBE_API_KEY' with your actual YouTube API key
     const apiKey = 'YOUR_YOUTUBE_API_KEY';
-    const videoId = 'Xw0WKPwQumw'; // Replace with your desired video's ID
-    const maxResults = 20; // Number of recommended videos to fetch
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&maxResults=${maxResults}&type=video&key=${apiKey}`;
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&relatedToVideoId=Xw0WKPwQumw&maxResults=9&key=${apiKey}`;
 
     this.http.get<any>(apiUrl).subscribe(
       (response) => {
-        const recommendedVideos = response.items.map((item: any) => {
+        const items = response.items;
+        this.videoList = items.map((item: any) => {
           const videoId = item.id.videoId;
           const videoUrl = `https://www.youtube.com/embed/${videoId}`;
-
           return {
             title: item.snippet.title,
-            url: this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl)
+            url: this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl),
           };
         });
-
-        // Randomly pick 3 videos from the recommendedVideos list
-        this.videoList = this.getRandomVideos(recommendedVideos, 3);
+        this.rollVideo(); // Call rollVideo() after updating the videoList
       },
       (error) => {
-        console.error('Failed to fetch recommended videos:', error);
+        console.error('Failed to fetch videos:', error);
       }
     );
   }
 
-  // Function to pick 'count' random videos from the video list
-  private getRandomVideos(videos: Video[], count: number): Video[] {
-    const randomVideos: Video[] = [];
-    const videoCount = videos.length;
-
-    while (randomVideos.length < count && randomVideos.length < videoCount) {
-      const randomIndex = Math.floor(Math.random() * videoCount);
-      const randomVideo = videos[randomIndex];
-
-      if (!randomVideos.includes(randomVideo)) {
-        randomVideos.push(randomVideo);
-      }
-    }
-
-    return randomVideos;
-  }
-
   rollVideo(): void {
-    this.fetchRecommendedVideos();
+    // Generate a random index to pick a video from the list
+    this.videoIndex = Math.floor(Math.random() * this.videoList.length);
   }
 }
-

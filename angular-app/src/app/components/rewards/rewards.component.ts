cf838dbdc92f488448ac293004279a84128b3adb
ED0LEO <ed0leo@proton.me>
2023-07-20 20:41:03 +0200

    Now videos are fetch on demand and fetched list is stored locally

--- angular-app/src/app/components/rewards/rewards.component.ts
+++ angular-app/src/app/components/rewards/rewards.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 interface Video {
   title: string;
   url: SafeResourceUrl;
-  thumbnailUrl: SafeResourceUrl; // Add thumbnail URL property
+  thumbnailUrl: SafeResourceUrl;
 }
 
 const presetVideoIds = [
@@ -37,21 +37,77 @@ export class RewardsComponent implements OnInit {
   isRolling: boolean = false;
   stopIndex: number | null = null;
   showThumbnails: boolean = true;
+  fetchLimit: number = 0;
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    this.fetchRecommendedVideos();
+    this.initializeVideoList();
+    this.updateFetchLimit();
+  }
+
+  decrementFetchLimit(): void {
+    if (this.fetchLimit > 0) {
+      this.fetchLimit--;
+      localStorage.setItem('fetchLimit', this.fetchLimit.toString());
+    }
+  }
+
+  updateFetchLimit(): void {
+    const lastUpdateTimestamp = localStorage.getItem('fetchLimitUpdate');
+    if (!lastUpdateTimestamp) {
+      // Set the initial fetch limit to 85 if not updated before
+      this.fetchLimit = 85;
+      localStorage.setItem('fetchLimit', this.fetchLimit.toString());
+    } else {
+      // Calculate the time difference in milliseconds
+      const currentTime = new Date().getTime();
+      const timeDiff = currentTime - Number(lastUpdateTimestamp);
+
+      // Check if a day has passed (86400000 ms = 1 day)
+      if (timeDiff >= 86400000) {
+        // Update fetch limit and store the new timestamp
+        this.fetchLimit = 85;
+        localStorage.setItem('fetchLimit', this.fetchLimit.toString());
+        localStorage.setItem('fetchLimitUpdate', currentTime.toString());
+      } else {
+        // Get the fetch limit from localStorage and update the local variable
+        const storedFetchLimit = localStorage.getItem('fetchLimit');
+        if (storedFetchLimit) {
+          this.fetchLimit = Number(storedFetchLimit);
+        } else {
+          // Set the initial fetch limit to 85 if not found in localStorage
+          this.fetchLimit = 85;
+          localStorage.setItem('fetchLimit', this.fetchLimit.toString());
+        }
+      }
+    }
+  }
+
+  initializeVideoList(): void {
+    // Check if videoList is stored locally in localStorage
+    const storedVideoList = localStorage.getItem('videoList');
+    if (storedVideoList) {
+      this.videoList = JSON.parse(storedVideoList, (key, value) => {
+        if (key === 'url' || key === 'thumbnailUrl') {
+          // Convert the string URLs back to SafeResourceUrl objects
+          return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+        }
+        return value;
+      });
+    } else if (this.fetchLimit > 0) {
+      // Fetch the video list only if it's not stored locally and fetchLimit is not zero
+      this.fetchRecommendedVideos();
+    }
   }
 
   fetchRecommendedVideos(): void {
     // Replace 'YOUR_YOUTUBE_API_KEY' with your actual YouTube API key
     const apiKey = 'YOUR_YOUTUBE_API_KEY';
-    const amountOfResults = 3;
+    const amountOfResults = 30;
 
     // Select a random video ID from the preset list
     const randomVideoId = presetVideoIds[Math.floor(Math.random() * presetVideoIds.length)];
-
 
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&relatedToVideoId=${randomVideoId}&maxResults=${amountOfResults}&key=${apiKey}`;
 
@@ -68,6 +124,18 @@ export class RewardsComponent implements OnInit {
             thumbnailUrl: this.sanitizer.bypassSecurityTrustResourceUrl(thumbnailUrl),
           };
         });
+
+         // Store the videoList in localStorage for future use
+        const storedVideoList = JSON.stringify(this.videoList, (key, value) => {
+          if (key === 'url' || key === 'thumbnailUrl') {
+            // Convert SafeResourceUrl objects to regular strings before storing
+            return value.changingThisBreaksApplicationSecurity;
+          }
+          return value;
+        });
+        localStorage.setItem('videoList', storedVideoList);
+
+        this.decrementFetchLimit();
       },
       (error) => {
         console.error('Failed to fetch videos:', error);

8c9950dddff5d1d36005cfd1a23a8df6f437f789
ED0LEO <ed0leo@proton.me>
2023-07-25 22:04:44 +0200

    Forms' colors now aren't blended

--- angular-app/src/app/components/dashboard/dashboard.component.ts
+++ angular-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
 
 @Component({
@@ -11,28 +10,14 @@ import { User } from '../../models/user.model';
 export class DashboardComponent implements OnInit {
   user: User | undefined;
 
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    // Fetch the current user's data
-    this.fetchUserData();
-  }
-
-  fetchUserData(): void {
-    if (this.authService.isLoggedIn) {
-      const userId = this.authService.getUserId();
-
-      if (userId !== undefined) {
-        // Fetch the user data using the UserService
-        this.userService.getUserById(userId).subscribe(
-          (user) => {
-            this.user = user;
-          },
-          (error) => {
-            console.error('Failed to fetch user data:', error);
-          }
-        );
-      }
-    }
+    // Subscribe to the user$ observable to get user data
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+    });
+    // Fetch the user data
+    this.authService.autoSetUpFromToken();
   }
 }

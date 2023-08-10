43b1eee47cc1aa9bedcb9ea92f5378e88e4d4de6
ED0LEO <ed0leo@proton.me>
2023-08-10 23:58:30 +0200

    Tasks map is used to find all days with completed tasks

--- angular-app/src/app/components/calendar/calendar.component.ts
+++ angular-app/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDatepickerInputEvent, MatCalendarCellClassFunction } from '@angular/material/datepicker';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../models/Task';
@@ -10,21 +10,53 @@ import { TaskDetailsComponent } from '../task-details/task-details.component';
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
   selectedDate: Date | null = null;
   completedTasks: Task[] = [];
+  completedTasksMap: Map<string, Task[]> = new Map();
 
   constructor(
     private taskService: TaskService,
     public dialog: MatDialog
   ) {}
 
+  ngOnInit(): void {
+    this.fetchCompletedTasks();
+  }
+
+  async fetchCompletedTasks(): Promise<void> {
+    // Get the current date to determine the month
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+
+    // Initialize the map
+    this.completedTasksMap = new Map<string, Task[]>();
+
+    // Loop through each day of the current month
+    for (let day = 1; day <= new Date(currentYear, currentMonth + 1, 0).getDate(); day++) {
+      // Construct the formatted date for the current day
+      const formattedDate = `${currentYear}-${(currentMonth + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+
+      // Fetch completed tasks for the current day
+      const tasks = await this.taskService.getCompletedTasksByDate(formattedDate).toPromise();
+
+      // Add tasks to the map only if the tasks array is defined
+      if (tasks !== undefined) {
+        this.completedTasksMap.set(formattedDate, tasks);
+      }
+    }
+  }
+
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
-  //Highligh 1st and 20th day
     if (view === 'month') {
-      cellDate = new Date(cellDate);
-      const date = cellDate.getDate();
-      return (date === 1 || date === 20) ? 'special-days' : '';
+      const formattedDate = this.formatDate(new Date(cellDate));
+
+      // Check if there are completed tasks for the current day
+      const tasksForDay = this.completedTasksMap.get(formattedDate);
+      const hasCompletedTasks = tasksForDay && tasksForDay.length > 0;
+
+      return hasCompletedTasks ? 'special-days' : '';
     }
     return '';
   }
@@ -32,7 +64,7 @@ export class CalendarComponent {
   selectDate(): void {
     if (this.selectedDate) {
       this.selectedDate = new Date(this.selectedDate);
-      const completionDate =this.formatDate(this.selectedDate);
+      const completionDate = this.formatDate(this.selectedDate);
       this.taskService.getCompletedTasksByDate(completionDate).subscribe((tasks) => {
         this.completedTasks = tasks;
       });

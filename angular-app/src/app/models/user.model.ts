62b9f7a8972946b55ff1f57018f19f787ae9921b
ED0LEO <ed0leo@proton.me>
2023-08-15 23:53:33 +0200

    User model now linked to tasks, habits and activities

--- angular-app/src/app/models/user.model.ts
+++ angular-app/src/app/models/user.model.ts
@@ -1,9 +1,16 @@
+import { Task } from './task';
+import { Habit } from './habit.model';
+import { Activity } from './activity.model';
+
 export class User {
-    id!: number;
-    firstName!: string;
-    lastName!: string;
-    username!: string;
-    password!: string;
-    sourceVideos!: string[];
-    points!: number;
+  id!: number;
+  firstName!: string;
+  lastName!: string;
+  username!: string;
+  password!: string;
+  sourceVideos!: string[];
+  points!: number;
+  tasks?: Task[];
+  habits?: Habit[];
+  dayPlanner?: Activity[];
 }

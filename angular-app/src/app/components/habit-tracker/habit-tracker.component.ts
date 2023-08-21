2493ba215a9e90a248a9489e318a1386b9a55fab
ED0LEO <ed0leo@proton.me>
2023-08-21 23:20:07 +0200

    Habit tracker how add habits correctly

--- angular-app/src/app/components/habit-tracker/habit-tracker.component.ts
+++ angular-app/src/app/components/habit-tracker/habit-tracker.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HabitService } from '../../services/habit.service'; // Import your habit service
+import { Habit } from '../../models/habit.model';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
+import { MatDialog } from '@angular/material/dialog';
+// import { HabitDetailsComponent } from '../habit-details/habit-details.component'; // Import your habit details component
 
 @Component({
   selector: 'app-habit-tracker',
@@ -6,20 +13,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./habit-tracker.component.css']
 })
 export class HabitTrackerComponent implements OnInit {
-  habits: { name: string; days: number }[] = [];
+  habits: Habit[] = [];
+  newHabit: Habit = new Habit();
+  user: User | undefined;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private habitService: HabitService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    // Initialize habits (e.g., with some example habits)
-    this.habits = [
-      { name: 'Smoking', days: 10 },
-      { name: 'Sugary snacks', days: 5 },
-      { name: 'Social media', days: 2 }
-    ];
+    this.fetchUserData();
+
+    if (this.user) {
+      this.fetchUserHabits();
+    }
   }
 
-  incrementDays(habit: { name: string; days: number }): void {
-    habit.days++;
+  fetchUserData(): void {
+    if (this.authService.isLoggedIn) {
+      const userId = this.authService.getUserId();
+
+      if (userId !== undefined) {
+        this.userService.getUserById(userId).subscribe(
+          (user) => {
+            this.user = user;
+          },
+          (error) => {
+            console.error('Failed to fetch user data:', error);
+          }
+        );
+      }
+    }
+  }
+
+  fetchUserHabits(): void {
+    if (this.user) {
+      this.habitService.getHabitsByUser(this.user.id).subscribe(habits => {
+        this.habits = habits;
+      });
+    }
+  }
+
+  openHabitDetails(habit: Habit): void {
+//     const dialogRef = this.dialog.open(HabitDetailsComponent, {
+//       data: habit
+//     });
+//
+//     dialogRef.afterClosed().subscribe(result => {
+//       if (result === 'updated') {
+//         // Habit was updated, fetch user habits again
+//         this.fetchUserHabits();
+//       }
+//     });
+  }
+
+  addHabit(): void {
+    if (this.user) {
+      this.newHabit.user = this.user;
+      this.habitService.createHabit(this.newHabit).subscribe(habit => {
+        this.habits.push(habit);
+        this.newHabit = new Habit();
+
+//         this.notificationService.showNotification('Habit created successfully!');
+      });
+    }
+  }
+
+  deleteHabit(habit: Habit): void {
+    if (habit.id && this.user) {
+      this.habitService.deleteHabit(habit.id).subscribe(() => {
+        // Habit was deleted, fetch user habits again
+        this.fetchUserHabits();
+      });
+    }
   }
 }

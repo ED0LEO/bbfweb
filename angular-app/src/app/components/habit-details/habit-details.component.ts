11d29adacbe895c59370e70880e7bc75eb03d693
ED0LEO <ed0leo@proton.me>
2023-08-25 22:34:32 +0200

    Habit details implemented

--- angular-app/src/app/components/habit-details/habit-details.component.ts
+++ angular-app/src/app/components/habit-details/habit-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Habit } from '../../models/habit.model';
+import { HabitService } from '../../services/habit.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-habit-details',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./habit-details.component.css']
 })
 export class HabitDetailsComponent {
+  editedHabit: Habit = { ...this.data }; // Create a copy of the habit data for editing
 
+  constructor(
+    public dialogRef: MatDialogRef<HabitDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Habit,
+    private habitService: HabitService,
+    private notificationService: NotificationService
+  ) {}
+
+  updateHabit(): void {
+    this.habitService.updateHabit(this.editedHabit).subscribe(
+      () => {
+        this.notificationService.showNotification('Habit updated successfully!');
+        this.dialogRef.close('updated'); // Notify parent component that habit was updated
+      },
+      (error) => {
+        console.error('Error updating habit:', error);
+      }
+    );
+  }
+
+  cancel(): void {
+    this.dialogRef.close(); // Close the dialog without saving changes
+  }
 }
